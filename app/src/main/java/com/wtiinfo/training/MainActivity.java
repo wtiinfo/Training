e5dbe1a59e641c2b089e1177c3d0ca67321bdcf8
Wando Borges <wanbrwan@gmail.com>
2022-08-02 15:21:35 -0300

    Fragmentos Dinânimicos

--- app/src/main/java/com/wtiinfo/training/MainActivity.java
+++ app/src/main/java/com/wtiinfo/training/MainActivity.java
@@ -1,6 +1,8 @@
 package com.wtiinfo.training;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        OneFragment one = new OneFragment();
+        TwoFragment two = new TwoFragment();
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(R.id.framelayout_one, one, "Frag_ONE");
+        ft.add(R.id.framelayout_two, two, "Frag_TWO");
+        ft.commit();
     }
 }
