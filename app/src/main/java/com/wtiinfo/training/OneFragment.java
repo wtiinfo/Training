ce2959efe9fba3d6c401c6211c03fb28b83a8510
Wando Borges <wanbrwan@gmail.com>
2022-08-02 15:10:54 -0300

    Fragmentos estáticos

---
+++ app/src/main/java/com/wtiinfo/training/OneFragment.java
@@ -0,0 +1,18 @@
+package com.wtiinfo.training;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class OneFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.one, container, false);
+    }
+}
